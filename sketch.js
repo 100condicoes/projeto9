fba43beed163cd2206cb494ee53e2a556abd5475
100condicoes <92954191+100condicoes@users.noreply.github.com>
2021-10-21 18:25:37 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,78 @@
+
+
+var quadrado;
+
+
+
+
+
+
+
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+  quadrado=createSprite(180,200,10,10)
+}
+
+function draw() 
+{quadrado.velocityX= 0
+ quadrado.velocityY= 0 
+  background(30);
+ 
+ if(keyDown("space")){
+background("red")
+ 
+
+
+
+}
+  
+  
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ drawSprites();
+}
+
+
+
